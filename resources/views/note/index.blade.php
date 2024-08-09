4db18ff5421adc9858dd973d1f190357a9004f65
Christian Nathaniel <114709222+ChristianNathanielP@users.noreply.github.com>
2024-08-09 14:20:14 +0700

    Notes

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -7,22 +7,24 @@
         </div>
         <div class="grid xl:grid-cols-3 md:grid-cols-1 lg:grid-cols-2 mt-10 gap-x-5 gap-y-12">
             @foreach ($notes as $note)
-            <div class="lg:w-[400px]  h-[350px] bg-biru1 px-3 bayangan2">
-                <div class="text-right py-1.5 text-kuning">
-                    {{ $note->updated_at->diffForHumans() }}
-                </div>
-                <div class="bg-biru2 rounded-md px-5 lg:py-5 py-3 text-base h-64 text-putih overflow-hidden">
-                    {{ Str::limit(strip_tags($note->note), 450) }}
-                </div>
-                <div class="flex justify-between text-lg">
-                    <a href="{{ route('note.show', $note) }}" class="px-8 py-2.5 hover:text-white transition duration-300">View</a>
-                    <a href="{{ route('note.edit', $note) }}" class="px-8 py-2.5 hover:text-green-300 transition duration-300">Edit</a>
-                    <form action="{{ route('note.destroy', $note) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button class="px-8 py-2.5 hover:text-red-300 transition duration-300">Delete</button>
-                    </form>
-                </div>
+            <div class="lg:w-[400px]  h-[350px] bg-biru1 px-3 bayangan2 transition duration-300 ease-in-out active-a">
+                <a href="{{ route('note.show', $note) }}">
+                    <div class="text-right py-1.5 text-kuning">
+                        {{ $note->updated_at->diffForHumans() }}
+                    </div>
+                    <div class="bg-biru2 rounded-md px-5 lg:py-5 py-3 text-base h-64 text-putih overflow-hidden">
+                        {{ Str::limit(strip_tags($note->note), 450) }}
+                    </div>
+                    <div class="flex justify-between text-lg">
+                        <a href="{{ route('note.show', $note) }}" class="px-8 py-2.5 hover:text-white transition duration-300">View</a>
+                        <a href="{{ route('note.edit', $note) }}" class="px-8 py-2.5 hover:text-green-300 transition duration-300">Edit</a>
+                        <form action="{{ route('note.destroy', $note) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button class="px-8 py-2.5 hover:text-red-300 transition duration-300">Delete</button>
+                        </form>
+                    </div>
+                </a>
             </div>
             @endforeach
         </div>
